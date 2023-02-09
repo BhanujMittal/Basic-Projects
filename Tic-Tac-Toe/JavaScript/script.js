f5c48239ffb32ae220b220df1af020a502a15ff4
Bhanuj <bhanujmittal2684@gmail.com>
2023-02-09 12:52:14 +0530

    removed extra code

--- Tic-Tac-Toe/JavaScript/script.js
+++ Tic-Tac-Toe/JavaScript/script.js
@@ -14,7 +14,7 @@ function Reload() {
 
 // main funciton which declares the winner
 function myFunc() {
-    var b1, b2, b3, b4, b5, b6, b7, b8, b9;
+    let b1, b2, b3, b4, b5, b6, b7, b8, b9;
     b1 = document.getElementsByClassName('box')[0].value
     b2 = document.getElementsByClassName('box')[1].value
     b3 = document.getElementsByClassName('box')[2].value
@@ -27,193 +27,81 @@ function myFunc() {
 
     if (b1 == 'X' && b2 == 'X' && b3 == 'X') {
         window.alert('Player X Won')
-
-        document.getElementById('b4').disabled = "true"
-        document.getElementById('b5').disabled = "true"
-        document.getElementById('b6').disabled = "true"
-        document.getElementById('b7').disabled = "true"
-        document.getElementById('b8').disabled = "true"
-        document.getElementById('b9').disabled = "true"
         Reload()
     }
 
     else if (b1 == 'X' && b4 == 'X' && b7 == 'X') {
         window.alert('Player X Won')
-
-        document.getElementById('b2').disabled = "true"
-        document.getElementById('b3').disabled = "true"
-        document.getElementById('b5').disabled = "true"
-        document.getElementById('b6').disabled = "true"
-        document.getElementById('b8').disabled = "true"
-        document.getElementById('b9').disabled = "true"
         Reload()
     }
 
     else if (b1 == 'X' && b5 == 'X' && b9 == 'X') {
         window.alert('Player X Won')
-
-        document.getElementById('b2').disabled = "true"
-        document.getElementById('b3').disabled = "true"
-        document.getElementById('b4').disabled = "true"
-        document.getElementById('b6').disabled = "true"
-        document.getElementById('b7').disabled = "true"
-        document.getElementById('b8').disabled = "true"
         Reload()
     }
 
     else if (b2 == 'X' && b5 == 'X' && b8 == 'X') {
         window.alert('Player X Won')
-
-        document.getElementById('b1').disabled = "true"
-        document.getElementById('b3').disabled = "true"
-        document.getElementById('b4').disabled = "true"
-        document.getElementById('b6').disabled = "true"
-        document.getElementById('b7').disabled = "true"
-        document.getElementById('b9').disabled = "true"
         Reload()
     }
 
     else if (b3 == 'X' && b6 == 'X' && b9 == 'X') {
         window.alert('Player X Won')
-
-        document.getElementById('b1').disabled = "true"
-        document.getElementById('b2').disabled = "true"
-        document.getElementById('b4').disabled = "true"
-        document.getElementById('b5').disabled = "true"
-        document.getElementById('b7').disabled = "true"
-        document.getElementById('b8').disabled = "true"
         Reload()
     }
 
     else if (b3 == 'X' && b5 == 'X' && b7 == 'X') {
         window.alert('Player X Won')
-
-        document.getElementById('b1').disabled = "true"
-        document.getElementById('b2').disabled = "true"
-        document.getElementById('b4').disabled = "true"
-        document.getElementById('b6').disabled = "true"
-        document.getElementById('b8').disabled = "true"
-        document.getElementById('b9').disabled = "true"
         Reload();
     }
 
     else if (b4 == 'X' && b5 == 'X' && b6 == 'X') {
         window.alert('Player X Won')
-
-        document.getElementById('b1').disabled = "true"
-        document.getElementById('b2').disabled = "true"
-        document.getElementById('b3').disabled = "true"
-        document.getElementById('b7').disabled = "true"
-        document.getElementById('b8').disabled = "true"
-        document.getElementById('b9').disabled = "true"
         Reload()
     }
 
     else if (b7 == 'X' && b8 == 'X' && b9 == 'X') {
         window.alert('Player X Won')
-
-        document.getElementById('b1').disabled = "true"
-        document.getElementById('b2').disabled = "true"
-        document.getElementById('b3').disabled = "true"
-        document.getElementById('b4').disabled = "true"
-        document.getElementById('b5').disabled = "true"
-        document.getElementById('b6').disabled = "true"
         Reload()
     }
 
     else if (b1 == '0' && b2 == '0' && b3 == '0') {
         window.alert('Player 0 Won')
-
-        document.getElementById('b4').disabled = "true"
-        document.getElementById('b5').disabled = "true"
-        document.getElementById('b6').disabled = "true"
-        document.getElementById('b7').disabled = "true"
-        document.getElementById('b8').disabled = "true"
-        document.getElementById('b9').disabled = "true"
         Reload()
     }
 
     else if (b1 == '0' && b4 == '0' && b7 == '0') {
         window.alert('Player 0 Won')
-
-        document.getElementById('b2').disabled = "true"
-        document.getElementById('b3').disabled = "true"
-        document.getElementById('b5').disabled = "true"
-        document.getElementById('b6').disabled = "true"
-        document.getElementById('b8').disabled = "true"
-        document.getElementById('b9').disabled = "true"
         Reload()
     }
 
     else if (b1 == '0' && b5 == '0' && b9 == '0') {
         window.alert('Player 0 Won')
-
-        document.getElementById('b2').disabled = "true"
-        document.getElementById('b3').disabled = "true"
-        document.getElementById('b4').disabled = "true"
-        document.getElementById('b6').disabled = "true"
-        document.getElementById('b7').disabled = "true"
-        document.getElementById('b8').disabled = "true"
         Reload()
     }
 
     else if (b2 == '0' && b5 == '0' && b8 == '0') {
         window.alert('Player 0 Won')
-
-        document.getElementById('b1').disabled = "true"
-        document.getElementById('b3').disabled = "true"
-        document.getElementById('b4').disabled = "true"
-        document.getElementById('b6').disabled = "true"
-        document.getElementById('b7').disabled = "true"
-        document.getElementById('b9').disabled = "true"
         Reload()
     }
 
     else if (b3 == '0' && b6 == '0' && b9 == '0') {
         window.alert('Player 0 Won')
-
-        document.getElementById('b1').disabled = "true"
-        document.getElementById('b2').disabled = "true"
-        document.getElementById('b4').disabled = "true"
-        document.getElementById('b5').disabled = "true"
-        document.getElementById('b7').disabled = "true"
-        document.getElementById('b8').disabled = "true"
         Reload()
     }
 
     else if (b3 == '0' && b5 == '0' && b7 == '0') {
         window.alert('Player 0 Won')
-
-        document.getElementById('b1').disabled = "true"
-        document.getElementById('b2').disabled = "true"
-        document.getElementById('b4').disabled = "true"
-        document.getElementById('b6').disabled = "true"
-        document.getElementById('b8').disabled = "true"
-        document.getElementById('b9').disabled = "true"
         Reload()
     }
 
     else if (b4 == '0' && b5 == '0' && b6 == '0') {
         window.alert('Player 0 Won')
-
-        document.getElementById('b1').disabled = "true"
-        document.getElementById('b2').disabled = "true"
-        document.getElementById('b3').disabled = "true"
-        document.getElementById('b7').disabled = "true"
-        document.getElementById('b8').disabled = "true"
-        document.getElementById('b9').disabled = "true"
         Reload()
     }
 
     else if (b7 == '0' && b8 == '0' && b9 == '0') {
         window.alert('Player 0 Won')
-
-        document.getElementById('b1').disabled = "true"
-        document.getElementById('b2').disabled = "true"
-        document.getElementById('b3').disabled = "true"
-        document.getElementById('b4').disabled = "true"
-        document.getElementById('b5').disabled = "true"
-        document.getElementById('b6').disabled = "true"
         Reload()
     }
 
